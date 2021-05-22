5b577f337b206a1ab5a5cbcffc12e7a14239b9ff
Arun Patra <arunkpatra@gmail.com>
2021-05-22 13:27:22 +0530

    Initial commit

---
+++ example/reloadly_sdk_dart_example.dart
@@ -0,0 +1,6 @@
+import 'package:reloadly_sdk_dart/reloadly_sdk_dart.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/reloadly_sdk_dart.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library reloadly_sdk_dart;
+
+export 'src/reloadly_sdk_dart_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
